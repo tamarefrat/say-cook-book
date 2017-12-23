7170c392dad2603a660e0f94108eec3d726faf65
Devorah <32932396+Devorah1@users.noreply.github.com>
2017-12-24 00:34:57 +0200

    Upgrade to angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { RecipeButtonComponent } from './recipe-button/recipe-button.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
-import {AngularFireDatabaseModule} from 'angularfire2/database'
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipeButtonComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
